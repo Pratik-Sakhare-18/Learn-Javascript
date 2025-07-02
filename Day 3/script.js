b36e0f69292469ab7350a80b32b8938203912b92
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-07-02 19:43:47 +0530

    learn Javascript

---
+++ Day 3/script.js
@@ -0,0 +1,80 @@
+
+// 1 . Arithmetic Operator 
+// +, -, *, /, %
+
+// +
+let a=10
+let b=20
+console.log(a+b)
+
+//-
+let c=10
+let d=20
+console.log(c-d)
+
+//*
+let e=10
+let f=20
+console.log(e*f)
+
+// /
+let g=10
+let h=2
+console.log(g/h)
+
+// %
+let i=10
+let j=3
+console.log(i%j)
+
+//2  . Increament & Decreament Operators { pre-incresment : (++a) , post-increament : (a++) } { pre-decreament : (--a) , post-decreament : (a--) }
+// ++ , --
+let abc=10
+console.log(abc++)
+console.log(abc--)
+console.log(++abc)
+console.log(--abc)
+
+
+// 3 . Assigenment Operator 
+// = , += , -= , /= , %=
+let assi = 3
+console.log(assi)
+
+// += etc 
+assi += 1
+console.log(assi)
+
+assi -= 1
+console.log(assi)
+
+assi *= 2
+console.log(assi)
+
+assi /= 2
+console.log(assi)
+
+assi %= 2
+console.log(assi)
+
+// 4 . comparison Operator
+// == , === , != , !== , > , < , >= , <=
+// ==
+let comp1 = 10
+let comp2 = "10"
+console.log(comp1 == comp2)
+
+// ===
+let comp3 = 10
+let comp4 = "10"
+console.log(comp3 === comp4)
+
+// !=
+let comp5 = 10
+let comp6 = "10"
+console.log(comp5 != comp6)
+
+// !==
+let comp7 = 10
+let comp8 = "10"
+console.log(comp7 !== comp8)
