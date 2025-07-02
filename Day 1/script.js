9d65d763eb9a04aa9511d2c6f2334eddb0d1094c
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-07-02 19:18:26 +0530

    learn javascript

---
+++ Day 1/script.js
@@ -0,0 +1,8 @@
+
+console.log("Hello JS") //this use to print to the console sereen
+console.warn("warning message") //this use to give warning message
+console.info("information message") //this use to give informetion 
+console.error("error message") //this use to give error message
+console.clear("") //this use is clear a a console screen 
+
+
---
+++ Day 2/script.js
@@ -0,0 +1,37 @@
+
+//data type in js
+
+// 1 . let
+// 2 . const 
+// 3 . var
+
+
+let name = "Pratik"
+const PI = 3.14
+var user_name = "Pratik Sakhare"
+
+console.log(name)
+console.log(PI)
+console.log(user_name)
+
+console.log(typeof name)
+console.log(typeof PI)
+console.log(typeof user_name)
+
+//boolean
+
+let isvaliduser = true
+let Is_OTP_Verified = false
+
+console.log(typeof isvaliduser)
+console.log(typeof Is_OTP_Verified)
+
+//undefine
+let Who_Win
+
+console.log(typeof Who_Win)
+
+//null
+
+let abc = null
+console.log(typeof abc)
