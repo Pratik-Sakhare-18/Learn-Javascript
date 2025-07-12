d7f8d39d87418c00b1dbf22e2c60f669e062ac87
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-07-12 15:27:06 +0530

    learn html

---
+++ Day 7/script.js
@@ -0,0 +1,32 @@
+let day = prompt("What day of the week is it today?")
+
+switch (day) {
+    case "sunday":
+        console.log("Today is Sunday");
+        break;
+
+    case "monday":
+        console.log("Today is Monday");
+        break;
+
+    case "tuesday":
+        console.log("Today is Tuesday");
+        break;
+
+    case "wednesday":
+        console.log("Today is Wednesday");
+        break;
+
+    case "thursday":
+        console.log("Today is Thursday");
+        break;
+
+    case "friday":
+        console.log("Today is Friday");
+        break;
+
+    case "saturday":
+        console.log("Today is Saturday");
+        break;
+
+}
