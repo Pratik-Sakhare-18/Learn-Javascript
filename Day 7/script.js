1d2a826ac7de2ed7c9c73b21c7a8cd5e8d241c66
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-07-13 17:32:54 +0530

    looping statement

--- Day 7/script.js
+++ Day 7/script.js
@@ -24,4 +24,24 @@ while(count <= 100)
     count += 2;
 }
 
+// print a odd no 0 to 100 using while loop
 
+let count1 = 1;
+
+while(count1 <= 100)
+{
+    console.log(count1);
+    count1 += 2;
+
+}
+
+// 2 . do while loop
+
+//  print a even no to 0 to 100 using do while loop
+
+let count2 = 0;
+
+do{
+    console.log(count2);
+    count2 += 2;
+}while(count2 <= 100);
