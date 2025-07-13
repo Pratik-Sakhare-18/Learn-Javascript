caf879395933d39b4087c5652293878f5727c211
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-07-13 17:38:28 +0530

    for loop

---
+++ Day 8/script.js
@@ -0,0 +1,16 @@
+let count = 0;
+for(let i = 0; i <= 10; i++)
+{
+    console.log(i);
+}
+
+
+// /print a even no of 0 to 100 using for loop
+
+let count1 = 0;
+
+for(let i = 0; i <= 100; i++)
+{
+    console.log(count1);
+    count1 = count1 + 2;
+}
