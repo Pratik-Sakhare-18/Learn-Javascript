0fe4938ab827063ab0891134d74f0213b2d8acf7
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-07-23 19:02:54 +0530

    DOM In JS

---
+++ Day 16/script.js
@@ -0,0 +1,15 @@
+// Document Object Model
+// weare access : -
+// 1 . accessing element
+// 2 . Modify the element 
+// 3 .  Handle Events
+// 4 . Controll Styling
+
+// 1 . accessing element
+
+let result = document.getElementsByTagName("h1")
+console.log(result[0])
+
+let result2 = document.getElementsByClassName("main-div")
+console.log(result2[0])
+
