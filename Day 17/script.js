23d5d0e81f8610c4e0372063fbddb9c5165bdeda
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-07-24 21:03:54 +0530

    DOM 17

---
+++ Day 17/script.js
@@ -0,0 +1,29 @@
+// get element by id
+
+let res = document.getElementById("#box");
+
+console.log(box)
+
+
+// query selector
+
+let des = document.querySelector("#container");
+console.log(des);
+
+// innertext
+
+des.innerText = "Hello Javascript";
+
+des.innerText="hello"
+
+console.log(res)
+
+// innerHml
+
+des.innerHTML="<h1>hii</h1>"
+
+// style change
+
+des.style.color="purple"
+des.style.backgroundColor="red"
+
