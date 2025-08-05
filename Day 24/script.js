2d303676be4303f5ff98bd7bab875d6f65b80918
Pratik-Sakhare-18 <sakharepratik37@gmail.com>
2025-08-05 19:14:20 +0530

    API & trycatch

---
+++ Day 24/script.js
@@ -0,0 +1,22 @@
+// API : is use to connectthe client or server weare use the api  to connect the frontebd and backend 
+
+async function loaddata() {
+    let responce = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await responce.json();
+    console.log(data);
+}
+
+loaddata();
+
+async function loaddata1() {
+    try {
+        let responce = await fetch("https://jsonplaceholder.typicode.com/users");
+        let data = await responce.json();
+        console.log(data);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
